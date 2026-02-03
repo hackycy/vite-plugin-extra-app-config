7ddea79190049d9bed3ac1ee0634e5f59274ccf6
Leo Yang <qa894178522@qq.com>
2026-02-03 16:58:19 +0800

    docs: update playground

--- playground/main.ts
+++ playground/main.ts
@@ -1,4 +1,4 @@
-import { getEnvConfig } from 'vite-plugin-extra-app-config/helper'
+import { useAppConfig } from 'vite-plugin-extra-app-config/helper'
 
 document.getElementById('app')!.innerHTML = '__UNPLUGIN__'
-console.log('Env Config:', getEnvConfig('__APP_ENV__'))
+console.log('Env Config:', useAppConfig(import.meta.env, import.meta.env.PROD, '__APP_ENV__'))
